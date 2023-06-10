748f6ddd8baffcf62bf0fce385fa7fce4b63ac50
Mishka Mebonia <nini@Mishkas-MacBook-Pro.local>
2023-06-10 13:52:43 +0400

    fixed main js bug

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import data from "./data.json" assert { type: 'json' }
 
 import { cards } from './scripts/renderCard.js'
 
-import { showCartProducts } from './scripts/cart'
+import { showCartProducts } from './scripts/cart.js'
 
 cards(data)
 
